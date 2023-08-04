26980a27cb5a4a4a558530ef781d90e3e125f63b
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-08-04 16:52:21 -0700

    Se agrega horario

--- includes/footer.php
+++ includes/footer.php
@@ -12,20 +12,29 @@
                     <div class="foot-col column column-1 text-center clearfix">
                         <div class="social-footer">
                             <ul>
-                                
+
                                 <li class="soc-icon">
-                                    <a target="_blank" href="https://www.facebook.com/profile.php?id=100086428643391"><i class="icon icon-themify-17"></i>Facebook</a>
+                                    <a target="_blank" href="https://www.facebook.com/profile.php?id=100086428643391"><i
+                                            class="icon icon-themify-17"></i>Facebook</a>
                                 </li>
-                                
+
                                 <li class="soc-icon">
-                                    <a target="_blank" href="https://www.instagram.com/baja_diablo/"><i class="icon icon-social-instagram"></i>Instagram</a>
+                                    <a target="_blank" href="https://www.instagram.com/baja_diablo/"><i
+                                            class="icon icon-social-instagram"></i>Instagram</a>
                                 </li>
                             </ul>
                         </div>
                     </div>
                     <div class="foot-col column column-1 text-center clearfix">
+                        <h3 style="color: #fff;">Horarios:</h3>
+                        <h5 style="color: #fff;">Lunes a Jueves: 10:00 am a 1:00 am</h5>
+                        <h5 style="color: #fff;"> Viernes, sábado y domingo: 10:00 am a 2:00 am</h5>
+                    </div>
+
+                    <div class="foot-col column column-1 text-center clearfix">
                         <div id="copyright" class="copyright-text">
-                            © Copyright 2023, Baja Diablo. Diseñado por <a style="color: #fff;" href="https://bananagroup.mx"><strong>Banana Group Marketing</strong></a>
+                            © Copyright 2023, Baja Diablo. Diseñado por <a style="color: #fff;"
+                                href="https://bananagroup.mx"><strong>Banana Group Marketing</strong></a>
                         </div>
                     </div>
                 </div>
